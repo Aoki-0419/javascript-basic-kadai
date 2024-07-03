7f7b816bca616da3e1e8402b4d4fcd50e5bceb0f
Aoki-0419 <164498097+Aoki-0419@users.noreply.github.com>
2024-07-03 16:51:42 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const addBtn = document.getElementById('btn');
+
+const parentElement = document.getElementById('text');
+
+//イベント処理の実行
+btn.addEventListener('click', () => {
+
+parentElement.textContent = 'ボタンをクリックしました';
+
+});
